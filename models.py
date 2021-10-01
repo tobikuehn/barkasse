86e812ba4ccdc1dce377310e90387578cafcea45
tobikuehn <post@tobikuehn.ch>
2021-10-01 21:15:34 +0200

    Houshold added

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class Transaction(models.Model):
     """A transaction of money"""
@@ -22,10 +23,30 @@ class Shop(models.Model):
         return self.name
 
 class Account(models.Model):
-    """A bank account we use to pay"""
+    """A bank account of an user we use to pay"""
     name = models.CharField(max_length=100, verbose_name="Konto")
     comment = models.TextField(null=True, verbose_name="Bemerkungen")
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    households = models.ManyToManyField('Household', through='HouseholdAccount')
 
     def __str__(self):
         """String for representing the Model object."""
         return self.name
+
+
+class Household(models.Model):
+    """A houshold groups accounts"""
+    name = models.CharField(max_length=100, verbose_name="Bezeichnung")
+    accounts = models.ManyToManyField('Account', through='HouseholdAccount')
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
+class HouseholdAccount(models.Model):
+    household = models.ForeignKey(Household, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.household + " / " + self.account
